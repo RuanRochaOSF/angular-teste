190c5b0b229a834c94698396921380369b86237a
Ruan <ruan.rocha@osf-global.com>
2019-05-14 14:25:10 -0700

    carousel v1

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, InputDecorator } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
 })
 export class CardComponent implements OnInit {
-  img: string = '../../../assets/AmazingSpiderMan545.jpg';
-  titulo: string = 'Spider-man - Infinito';
+  @Input() favorito: boolean;
+  @Input() comprado: boolean;
+  @Input() objeto: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
--- src/app/componentes/vitrine/vitrine.component.ts
+++ src/app/componentes/vitrine/vitrine.component.ts
@@ -1,16 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-vitrine',
-  templateUrl: './vitrine.component.html',
-  styleUrls: ['./vitrine.component.scss']
+  selector: "app-vitrine",
+  templateUrl: "./vitrine.component.html",
+  styleUrls: ["./vitrine.component.scss"]
 })
 export class VitrineComponent implements OnInit {
+  todosProdutos: Array<any> = [
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito1",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito2",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito3",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito4",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito5",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito6",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito7",
+      descricao: "bla bla bla bla"
+    },
+    {
+      src: "../../../assets/AmazingSpiderMan545.jpg",
+      titulo: "Spider-man - Infinito8",
+      descricao: "bla bla bla bla"
+    }
+  ];
+  produtosVisiveis: Array<any>;
 
-  produtos: Array<any> = [{nome:"Ruan"},{nome:"Ruan"},{nome:"Ruan"},{nome:"Ruan"},{nome:"Ruan"},{nome:"Ruan"},{nome:"Ruan"}];
-  constructor() { }
+  indiceIni: number;
+  indiceFin: number;
+  constructor() {}
 
   ngOnInit() {
+    this.indiceIni = 0;
+    this.todosProdutos.length < 4
+      ? (this.indiceFin = this.todosProdutos.length)
+      : (this.indiceFin = 4);
+    this.mostrar_cards();
   }
 
+  mostrar_cards() {
+    const cards = this.todosProdutos.slice(this.indiceIni, this.indiceFin);
+    this.produtosVisiveis = cards;
+  }
+
+  ir() {
+    if (this.indiceFin === this.todosProdutos.length) {
+      return;
+    }
+    this.indiceIni++;
+    this.indiceFin++;
+    this.mostrar_cards();
+  }
+  voltar(): void {
+    if (this.indiceIni === 0) {
+      return;
+    }
+    this.indiceIni--;
+    this.indiceFin--;
+    this.mostrar_cards();
+  }
 }
