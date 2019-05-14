fbc0c39bf86d6efcd88a1c4d4107d4e3e8944006
Ruan <ruan.rocha@osf-global.com>
2019-05-14 12:21:30 -0700

    olha as coisas melhorando

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  img: string = '../../../assets/AmazingSpiderMan545.jpg';
+  titulo: string = 'Spider-man - Infinito';
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
